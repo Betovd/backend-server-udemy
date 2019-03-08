9f5c7d81583191b55a719d8660ffafe3bd8263df
User <user@Users-MacBook-Pro.local>
2019-03-07 20:55:51 -0700

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,24 +1,51 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
-// Conección a la base de datos
-mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
-    if (err) throw err;
-    console.log('Base de datos online: \x1b[32m%s\x1b[0m', 'online');
-});
 
 // Inicializar variables
 var app = express();
 
+
+// Body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+// Exportar Rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
+// Conección a la base de datos
+// mongoose.connection('mongodb://localhost:27017/hospitalDB', { useNewUrlParser: true }.then(
+
+//     (err) => {
+
+//         if (err) throw err;
+//         console.log('Base de datos online: \x1b[32m%s\x1b[0m', 'online');
+//     }));
+
+
+
+// Or using promises
+mongoose.connect('mongodb://localhost:27017/hospitalDB', { useNewUrlParser: true, useCreateIndex: true, }).then(
+    () => { console.log('Base de datos online: \x1b[32m%s\x1b[0m', 'online'); },
+    err => { throw err }
+);
+
+
+
+
 // Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    });
-});
+app.use('/login', loginRoutes);
+app.use('/usuario', usuarioRoutes);
+app.use('/', appRoutes);
 
 
 // Escuchar peticiones
